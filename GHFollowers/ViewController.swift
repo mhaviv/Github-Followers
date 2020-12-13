b41d0eb6132a92b35735b932b6b6614fcb2956e2
Michael <mhaviv18@gmail.com>
2020-12-13 02:14:04 -0500

    First commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Michael Haviv on 12/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemTeal
+    }
+
+}
